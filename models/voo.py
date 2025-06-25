feb401aa3b4ab3a8194132bd21eff789b9cc4abd
MLuana725 <marialuana.sl962@gmail.com>
2025-06-25 19:11:43 -0300

    Criação de métodos envolvendo a administração dos assentos. Criação do objeto assento_disp para utilizar na função de checagem de disponibilidade de assentos e atualização do nome "assentos" para "assentos_total"

--- models/voo.py
+++ models/voo.py
@@ -1,22 +1,25 @@
 class Voo:
-    def __init__(self, numero_voo, preco, data_partida, data_chegada, assentos):
+    def __init__(self, numero_voo, preco, data_partida, data_chegada, assentos_total, reservar_assento):
         self.numero_voo = numero_voo
         self.preco = preco
         self.data_partida = data_partida
         self.data_chegada = data_chegada
-        self.assentos = assentos
+        self.assentos_total = assentos_total
+        self.assentos_disp = assentos_total
+        self.reservar_assento = reservar_assento
         pass 
 
     def __repr__(self):
-        return(f"Numero do voo(numero_voo={self.numero_voo}, preco='{self.preco}', data de partida='{self.data_partida}', "
-                f"data de chegada='{self.data_chegada}', assentos'{self.assentos}'")
+     return(f"Voo(numero_voo={self.numero_voo}, preco='{self.preco}', data_partida='{self.data_partida}', "
+            f"data_chegada='{self.data_chegada}', assentos_total={self.assentos_total}, assentos_disp={self.assentos_disp}")
+
     
     def to_dict(self):
         return{
         'Número do voo': self.numero_voo,
         'Data de partida': self.data_partida,              
         'Data de chegada': self.data_chegada,
-        'Número de assentos': self.assentos,
+        'Número de assentos': self.assentos_total,
         'Preço': self.preco
 
         }
@@ -27,9 +30,16 @@ class Voo:
            numero_voo = data['Número do voo'],
            data_partida = data['Data de partida'],
            data_chegada = data['Data de chegada'],
-           assentos = data['Número de assentos'],
+           assentos_total = data['Número de assentos'],
            preco = data['Preço']
         )
     
+    # Assentos
 
+    def assentos_disponiveis(self):
+        if self.assentos_disp == 0 :
+            print('Não existem assentos disponiveís'),
+        else:
+            print('Ainda há {self.assentos_disp} assentos disponiveís'),
+        pass
     
