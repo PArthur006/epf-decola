ce2a206ec73a9bc8832d65c7da33364364a8f99e
MLuana725 <marialuana.sl962@gmail.com>
2025-07-04 16:21:25 -0300

    metodo de salvamento dos dados de voo

--- models/voo.py
+++ models/voo.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Optional
-
+import json
+import os
 
 class Voo:
     def __init__(self, numero_voo, preco, data_partida, data_chegada, assentos_total):
@@ -102,8 +103,56 @@ class Voo:
             if self.assentos_disp > self.assentos_total:
                 self.assentos_disp = self.assentos_total
 
-   
-   
+DATA_DIR = os.path.join(os.path.dirname(__file__), '..', 'data')
+os.makedirs(DATA_DIR, exist_ok=True)
+
+class VooModel:
+    FILE_PATH = os.path.join(DATA_DIR, 'voos.json')
+
+    def __init__(self):
+        self.voos = self._load()
+
+    def _load(self):
+        if not os.path.exists(self.FILE_PATH):
+            return []
+        with open(self.FILE_PATH, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            for item in data:
+                item['Data de partida'] = datetime.fromisoformat(item['Data de partida'])
+                item['Data de chegada'] = datetime.fromisoformat(item['Data de chegada'])
+            return [Voo.from_dict(item) for item in data]
+
+    def _save(self):
+        with open(self.FILE_PATH, 'w', encoding='utf-8') as f:
+            json.dump([self._format_dates(v.to_dict()) for v in self.voos], f, ensure_ascii=False, indent=4)
+
+    def _format_dates(self, data_dict):
+        if isinstance(data_dict.get('Data de partida'), datetime):
+            data_dict['Data de partida'] = data_dict['Data de partida'].isoformat()
+        if isinstance(data_dict.get('Data de chegada'), datetime):
+            data_dict['Data de chegada'] = data_dict['Data de chegada'].isoformat()
+        return data_dict
+
+    def get_all(self):
+        return self.voos
+
+    def get_by_numero_voo(self, numero_voo):
+        return next((v for v in self.voos if v.numero_voo == numero_voo), None)
+
+    def add(self, voo: Voo):
+        self.voos.append(voo)
+        self._save()
+
+    def update(self, voo_atualizado: Voo):
+        for i, v in enumerate(self.voos):
+            if v.numero_voo == voo_atualizado.numero_voo:
+                self.voos[i] = voo_atualizado
+                self._save()
+                break
+
+    def delete(self, numero_voo):
+        self.voos = [v for v in self.voos if v.numero_voo != numero_voo]
+        self._save()  
 
 
 
