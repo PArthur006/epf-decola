83a4a27f02687da9dedd107c232ba56f74a4b806
MLuana725 <marialuana.sl962@gmail.com>
2025-07-04 16:04:15 -0300

    Construtores de pagamento adicionados

--- models/pagamento.py
+++ models/pagamento.py
@@ -1,3 +1,15 @@
+from datetime import datetime
+
 class Pagamento:
-    def __init__(self):
-        pass
+    def __init__(self, id_pagamento, numero_voo, valor, forma_pagamento, status, data_pagamento=None):
+        self.id_pagamento = id_pagamento
+        self.numero_voo = numero_voo
+        self.valor = valor
+        self.forma_pagamento = forma_pagamento  # Ex: 'Cartão', 'PIX', 'Boleto'
+        self.status = status                    # Ex: 'Pendente', 'Pago', 'Cancelado'
+        self.data_pagamento = data_pagamento or datetime.now()
+
+    def __repr__(self):
+        return (f"Pagamento(id_pagamento={self.id_pagamento}, numero_voo='{self.numero_voo}', "
+                f"valor={self.valor}, forma_pagamento='{self.forma_pagamento}', "
+                f"status='{self.status}', data_pagamento='{self.data_pagamento}')")
